c7f93f5370cb642f2bb7b7ca3227d1329910e92a
yessica ancan <yancalonzo@gmail.com>
2016-07-19 10:14:48 -0400

    Estructura y estilos Mi Pelicula

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 
+/*
+$(document).ready(function(){
+$(".Detalles").hide(); //Ocultar capas
+$("select.seleccionar2 option:first").addClass("activa").show(); //Activar primera pestaña
+$(".Detalles:first").show(); //Mostrar contenido primera pestaña
+
+// Sucesos al hacer click en una pestaña
+$("select.seleccionar2 option").click(function(){
+$("select.seleccionar2 option").removeClass("activa"); //Borrar todas las clases "activa"
+$(this).addClass("activa"); //Añadir clase "activa" a la pestaña seleccionada
+$(".Detalles").hide(); //Ocultar todo el contenido de la pestaña
+var activatab = $(this).find("a").attr("href"); //Leer el valor de href para identificar la pestaña activa 
+$(activatab).fadeIn(); //Visibilidad con efecto fade del contenido activo
+return false;
+});
+});*/
 
 
 
